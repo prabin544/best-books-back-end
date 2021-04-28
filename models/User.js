00bc2c8be19a76a453708cd67738abde924d68b4
prabin singh <pravin544@gmail.com>
2021-04-27 23:51:28 -0500

    added model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const BookSchema = new Schema({
+    name: String,
+    description: String,
+    status: Boolean,
+});
+
+const UserSchema = new Schema({
+  name: String,
+  email: String,
+  // favoriteActivities data type is an array of activitySchema
+  books: [BookSchema]
+});
+
+// make a model out of the schema
+const User = mongoose.model('User', UserSchema);
+
+// export the model
+module.exports = User;
--- server.js
+++ server.js
@@ -9,12 +9,42 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/best-books', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const User = require('./models/User')
+
+const newUser = new User({
+    name: 'Alex',
+    email: 'alex@gmail.com', 
+    books: [
+      {name: 'JS Expert'}, 
+      {description: 'Zero to Hero'}, 
+      {status: 'published'}
+    ]
+  });
 
 app.get('/', (request, response) => {
     // when we get that request, send a response that says 'hello!'
     // response has some methods that are very helpful, such as a send method
-    response.send('hello!');
+    response.send('hello! from best-books backend');
   });
+
+app.get('/users', (req, res) => {
+// get all the users from the database
+User.find((err, databaseResults) => {
+    // send them in my response
+    res.send(databaseResults);
+});
+});
+
+// route to get just one user
+app.get('/user', (req, res) => {
+User.find({name: req.query.name}, (err, databaseResults) => {
+    // send them in my response
+    res.send(databaseResults);
+});
+});
+
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`)); 
