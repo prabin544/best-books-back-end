f1727913887b9b0ea398e01e57bf56b3c5c2ee59
prabin singh <pravin544@gmail.com>
2021-04-28 22:36:59 -0500

    adding routes

--- server.js
+++ server.js
@@ -9,19 +9,29 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect('mongodb://localhost/best-books', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/books', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
 
 const User = require('./models/User')
 
 const newUser = new User({
-    name: 'Alex',
-    email: 'alex@gmail.com', 
+    name: 'Prabin',
+    email: 'pravin544@gmail.com', 
     books: [
-      {name: 'JS Expert'}, 
-      {description: 'Zero to Hero'}, 
-      {status: 'published'}
+      {name: ['JS Expert', 'Express Expert']}, 
+      {description: ['Zero to Hero', "Foundation"]}, 
+      {status: [true, false]}
+     
     ]
   });
+
+console.log({newUser})
+newUser.save();
 
 app.get('/', (request, response) => {
     // when we get that request, send a response that says 'hello!'
@@ -29,22 +39,26 @@ app.get('/', (request, response) => {
     response.send('hello! from best-books backend');
   });
 
-app.get('/users', (req, res) => {
-// get all the users from the database
-User.find((err, databaseResults) => {
-    // send them in my response
-    res.send(databaseResults);
-});
-});
+app.get('/books', getAllUsers)
 
-// route to get just one user
-app.get('/user', (req, res) => {
-User.find({name: req.query.name}, (err, databaseResults) => {
-    // send them in my response
-    res.send(databaseResults);
-});
-});
+function getAllUsers(request, response) {
+  const name = request.query.name;
+  console.log({name});
+  User.find({name}, (err, person) => {
+    if(err) return console.error(err);
+    console.log({person})
+    response.send(person[0].books);
+  })
+}
 
+// app.get('/books', (request, response) => {
+//   console.log("This should show books")
+//   User.find(function (err, users) {
+//     if (err) return console.error(err);
+//     console.log(users);
+//     response.send(users);
+//   })
 
+// });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`)); 
