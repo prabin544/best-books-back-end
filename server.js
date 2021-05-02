d43318b373d3dd975c1c000d81cb3f028212b85b
prabin singh <pravin544@gmail.com>
2021-05-01 23:02:47 -0500

    adding post

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect('mongodb://localhost/books', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -60,16 +61,49 @@ const secondUser = new User({
 
     ]
   })
-
-console.log({newUser})
 newUser.save();
 secondUser.save();
 
 app.get('/', (request, response) => {
-    response.send('hello! from best-books backend');
+    // response.send('hello! from best-books backend');
+    User.find((err, userData) => {
+      response.send(userData);
+    });
   });
 
+app.get('/users/:email', (request, response) => {
+  User.find({email: request.params.email},
+    (err, userData) => {
+      response.send(userData)
+    });
+});
+
 app.get('/books', getAllUsers)
+
+app.post('/addbooks', (req, res) => {
+  // for post requests, data is inside of the body
+  // as long as we have the app.use(express.json()) line at the top of the file
+  console.log(req.body);
+  // find the relevant user in the database
+  // Users.find({email: req.body.email}, (err, userData) => {
+  //   if(userData.length < 1) {
+  //     res.status(400).send('user does not exist');
+  //   } else {
+  //     // add the new gift info to that user
+  //     let user = userData[0];
+  //     user.books.push({
+  //       name: req.body.name,
+  //       description: req.body.description,
+  //       status: req.body.status
+  //     });
+  //     // save the user
+  //     user.save().then( (userData) => {
+  //       console.log(userData);
+  //       res.send(userData.gifts);
+  //     });
+  //   }
+  // });
+});
 
 function getAllUsers(request, response) {
   console.log(request)
